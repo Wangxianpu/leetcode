bfcd9e8d9f67237b7b0969298dbed57c50a32279
Wangxp <15361865170@163.com>
2018-10-23 14:59:41 +0800

    559. N叉树的最大深度 自顶向下深度优先遍历 另外一种解法更优：自底向上思考

--- src/main/java/com/wxp/tree/BinaryTreePaths.java
+++ src/main/java/com/wxp/tree/BinaryTreePaths.java
@@ -9,31 +9,38 @@ import java.util.List;
  */
 public class BinaryTreePaths {
 
-    List<String> ans ;
+     List<String> ans ;
 
-    public List<String> binaryTreePaths(TreeNode root) {
+    public  List<String> binaryTreePaths(TreeNode root) {
         ans = new ArrayList<String>();
         if(root == null ){
             return ans;
         }
-        if(root.left != null)
-        dfs(root.left,String.valueOf(root.val));
-        if(root.right != null)
-        dfs(root.right,String.valueOf(root.val));
-        if(root.left == null && root.right == null)
-        ans.add(String.valueOf(root.val));
+        dfs(root,"");
         return ans;
     }
 
-    private void dfs(TreeNode root,String route) {
-        route += "->"+String.valueOf(root.val);
+    private  void dfs(TreeNode root,String route) {
         if(root.left == null && root.right == null){
+            if("".equals(route)){
+                route += root.val;
+            }else{
+                route += "->" + root.val;
+            }
             ans.add(route);
             return;
         }
-        if(root.left != null)
-        dfs(root.left,route);
-        if(root.right != null)
-        dfs(root.right,route);
+        if("".equals(route)){
+            route += root.val;
+        }else{
+            route += "->" + root.val;
+        }
+        if(root.left != null) {
+            dfs(root.left,route);
+        }
+        if(root.right != null) {
+            dfs(root.right,route);
+        }
     }
+
 }
---
+++ src/main/java/com/wxp/tree/MaxDepthOfNTree.java
@@ -0,0 +1,43 @@
+package com.wxp.tree;
+
+import java.util.List;
+
+/**
+ * 559. N叉树的最大深度
+ * Created by wangxianpu on 2018/10/23.
+ */
+public class MaxDepthOfNTree {
+    
+    int max = 0;
+    
+    public int maxDepth(Node root) {
+        if(root == null)
+            return max;
+        dfs(root,1);
+        return max;
+    }
+
+    private void dfs(Node root,int deep) {
+        if(root.children == null || root.children.size() == 0){
+            if(deep > max){
+                max = deep;
+            }
+        }
+        deep++;
+        for(Node node : root.children){
+            dfs(node,deep);
+        }
+    }
+}
+
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val,List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
